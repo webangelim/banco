3d14256492d2325d1accba45b3c87fc8c68c0d0f
webangelim <web.angelim@gmail.com>
2026-07-10 16:08:54 -0300

    feat(usuario): add Usuario entity with validation

---
+++ src/main/java/com/angelim/banco/usuario/Usuario.java
@@ -0,0 +1,76 @@
+package com.angelim.banco.usuario;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @CPF
+    @Size(min = 11, max = 11)
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @Email
+    @Size(max = 255)
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false, length = 60)
+    private String senha;
+
+    public Usuario(UUID id, String cpf, String email, String senha) {
+        this.id = id;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Usuario() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
